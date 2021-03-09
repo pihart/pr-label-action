79d1b17b3393625d6e49219137788585fddc1f05
pihart <avi.mehra1@gmail.com>
2021-03-08 23:32:55 -0800

    Remove unnecessary logic

--- action.js
+++ action.js
@@ -2,22 +2,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { execSync } = require("child_process");
 
-const prNumber = ((github.context.payload || {}).pull_request || {}).number;
-
-if (prNumber == null) {
-  console.log("Could not get pull request number from context, exiting");
-  return;
-}
-
 const token = core.getInput("repo-token", { required: true });
 
 const client = new github.GitHub(token);
-
-const { data: pullRequest } = await client.pulls.get({
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo,
-  pull_number: prNumber,
-});
 
 /**
  * @type {{label: string, run: string}[]}
